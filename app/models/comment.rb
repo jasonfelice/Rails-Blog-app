777be69130818fd44024c05ca266989709034d54
Jake Felice <jasonfelice0@gmail.com>
2022-08-16 06:27:45 -0700

    Set up associations on comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,12 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_save :count_comments
+
+  private
+
+  def count_comments
+    post.increment(:comments_counter)
+  end
 end
