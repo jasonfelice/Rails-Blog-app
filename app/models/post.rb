a24ace2889d8d30fa0db33cd2c4316d8fc8a6a61
Jake Felice <jasonfelice0@gmail.com>
2022-08-16 06:32:22 -0700

    Set up associations on post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  after_save :count_post
+
+  private
+
+  def count_post
+    author.increment!(:posts_counter)
+  end
 end
