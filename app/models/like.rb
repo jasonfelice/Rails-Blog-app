b970f1b279254f12b0102598409fcba8fb9b9f6a
Jake Felice <jasonfelice0@gmail.com>
2022-08-16 06:29:16 -0700

    Set up associations on like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,12 @@
 class Like < ApplicationRecord
+  belongs_to :author, class_name :'User'
+  belongs_to :post
+
+  after_save :count_likes
+
+  private
+
+  def count_likes
+    post.increment!(:likes_counter)
+  end
 end
