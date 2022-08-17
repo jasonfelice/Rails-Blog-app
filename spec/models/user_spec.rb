85b1886c50c60425fbe962e05b848ef4ff28e894
Jake Felice <jasonfelice0@gmail.com>
2022-08-17 03:40:54 -0700

    Add numericality validation for 'post_counter'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,8 @@ RSpec.describe Like, type: :model do
   it 'Should be valid' do
     expect(subject).to be_valid
   end
+
+  it 'post_counter should be a greter than or equal to 0' do
+    expect(subject.posts_counter).to be >= 0
+  end
 end
