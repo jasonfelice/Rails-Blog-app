01827d21577b52e79df4bbbd0da15efb897503c9
Jake Felice <jasonfelice0@gmail.com>
2022-08-17 03:57:46 -0700

    Add tests for length validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -21,4 +21,21 @@ RSpec.describe Like, type: :model do
     subject.title = nil
     expect(subject).to_not be_valid
   end
+
+  it 'comments_counter should be a greter than or equal to 0' do
+    expect(subject.comments_counter).to be >= 0
+  end
+
+  it 'likes_counter should be a greter than or equal to 0' do
+    expect(subject.likes_counter).to be >= 0
+  end
+
+  it 'text should be less than 250 characters' do
+    subject.title = 'Lorem Ipsum is simply dummy text of the
+    printing and typesetting industry. Lorem Ipsum has been the industry
+    standard dummy text ever since the 1500s, when an unknown printer took a galley
+    of type and scrambled it to make a type specimen book. It has survived not only
+    five centuries'
+    expect(subject).to_not be_valid
+  end
 end
