b259a683190df4fd2856819faca444a605c9f6fa
Jake Felice <jasonfelice0@gmail.com>
2022-08-17 02:53:05 -0700

    Create specs for post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  subject do
+    user = User.create(name: 'Peter', photo: '_url_', bio: 'Brewer', posts_counter: 0)
+    post = Post.create(title: 'Test3', text: 'This is test', comments_counter: 0, likes_counter: 0, author_id: user.id)
+    Post.new(title: 'Test3', text: 'This is test 3', comments_counter: 0, likes_counter: 0, author_id: user.id)
+  end
+
+  before { subject.save }
+
+  it 'author_id should be present' do
+    subject.author_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Should be valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+end
