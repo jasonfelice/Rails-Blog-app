9fbc2ebe26165a591ca85df317b9b2696c217b96
Jake Felice <jasonfelice0@gmail.com>
2022-08-17 02:13:12 -0700

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0}
+  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
   after_save :count_post
 
