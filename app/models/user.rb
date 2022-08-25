58299b7f1f8c3ff600817814fef59b60e1096f06
Jake Felice <jasonfelice0@gmail.com>
2022-08-25 05:14:14 -0700

    Create user view

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def recent_posts
-    post.order(created_at: :DESC).limit(3)
+    posts.order(created_at: :DESC).limit(3)
   end
 end
